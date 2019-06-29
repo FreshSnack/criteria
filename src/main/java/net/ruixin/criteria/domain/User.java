591ce6220669ab53bcbdaa89987da80dd13e01aa
mxding <937652174@qq.com>
2019-06-29 23:15:03 +0800

    cannot simultaneously fetch multiple bags

--- src/main/java/net/ruixin/criteria/domain/User.java
+++ src/main/java/net/ruixin/criteria/domain/User.java
@@ -2,6 +2,8 @@ package net.ruixin.criteria.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.util.List;
@@ -21,7 +23,8 @@ public class User {
     @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
     @JoinColumn(name = "ORGAN_ID")
     private Organ organ;
-    @ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    @ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    @Fetch(FetchMode.SUBSELECT)
     @JoinTable(name="USER_ROLE", joinColumns = @JoinColumn(name="USER_ID", referencedColumnName = "ID"),
     inverseJoinColumns = @JoinColumn(name="ROLE_ID", referencedColumnName = "ID"))
     private List<Role> roleList;
